03669b4bd431b1db195359e0b05b9b4c0351ee76
Ernie Casilla <ecasilla@icloud.com>
2015-06-17 17:35:22 -0400

    adding polyfill support for ie with svg setAttribute

--- test/unit/classie_spec.js
+++ test/unit/classie_spec.js
@@ -1,12 +1,14 @@
+"use strict";
 /*jshint -W079, -W020 */
 /*global window, document, subject, exports,define: false, module: false */
 var classie = require('../../lib/classie.js'),
     fixtures = require('../fixtures/fixtures.js'),
+    context = describe,
     expect = require('chai').expect;
 
 
 describe('The new classie js', function() {
-    describe('adding a single class to an element', function() {
+    context('adding a single class to an element', function() {
         beforeEach(function() {
             var div = document.createElement('div');
             var section = document.createElement('section');
@@ -20,6 +22,10 @@ describe('The new classie js', function() {
         afterEach(function() {
             var body = document.body,
                 sections = document.querySelectorAll("section");
+            for (var i = 0, l = sections.length; i < l; i++) {
+                var v = sections[i];
+                body.removeChild(v);
+            }
         });
         it("it should be an object", function() {
             expect(classie).to.be.an('object');
@@ -33,7 +39,7 @@ describe('The new classie js', function() {
         it("should be in the global context", function() {
             expect(exports.classie).to.not.be.null;
         });
-        describe('The basic classie interface/api', function() {
+        context('The basic classie interface/api', function() {
 
             it('should be able to add a single class', function() {
                 classie.addClass(subject, 'hello');
@@ -55,7 +61,7 @@ describe('The new classie js', function() {
                 expect(subject.className).to.be.eql('');
             });
         });
-        describe('classie advanced features', function() {
+        context('classie advanced features', function() {
 
             it('should be able to take an array of classes', function() {
                 classie.addClass(subject, ['awesome', 'hello', 'classieIsAwesome']);
@@ -65,6 +71,44 @@ describe('The new classie js', function() {
             it('should be able to remove an array of classes', function() {
                 classie.removeClass(subject, ['awesome', 'hello', 'classieIsAwesome']);
                 expect(subject.className).to.eql('');
+            });
+        });
+        context('svg and prev vendor polyfills', function() {
+            it('should add a class to an svg element', function() {
+                var svgns = "http://www.w3.org/2000/svg";
+                var body = document.getElementsByTagName('body')[0];
+                // "circle" may be any tag name
+                var shape = document.createElementNS(svgns, "circle");
+                // Set any attributes as desired
+                shape.setAttribute("cx", 25);
+                shape.setAttribute("cy", 25);
+                shape.setAttribute("r", 20);
+                shape.setAttribute("fill", "green");
+                // Add to a parent node; document.documentElement should be the root svg element.
+                // Acquiring a parent element with document.getElementById() would be safest.
+                classie.addClass(shape, 'awesome');
+                body.appendChild(shape);
+                //finding the last element in the child nodes array
+                expect(body.childNodes[body.childNodes.length - 1]).to.be.equal(shape);
+                expect(shape.getAttribute('class')).to.equal('awesome');
+                body.removeChild(shape);
+            });
+            it('should remove a class to an svg element', function() {
+                var svgns = "http://www.w3.org/2000/svg";
+                var body = document.getElementsByTagName('body')[0];
+                // "circle" may be any tag name
+                var shape = document.createElementNS(svgns, "circle");
+                // Set any attributes as desired
+                shape.setAttribute("cx", 25);
+                shape.setAttribute("cy", 25);
+                shape.setAttribute("r", 20);
+                shape.setAttribute("fill", "green");
+
+                classie.addClass(shape, 'awesome');
+                body.appendChild(shape);
+                classie.removeClass(shape, 'awesome');
+                expect(shape.getAttribute('class')).to.equal('');
+                body.removeChild(shape);
             });
         });
     });
